6c994a1af9b13b56b9ac9b61b1bc0b8d4b58e222
Zura Gabievi <zura.gabievi@singular.ge>
2015-08-31 11:47:09 +0400

    v

--- alert.js
+++ alert.js
@@ -1,13 +1,35 @@
 (function ($) {
-	$.fn.alert = function () {
+	$.fn.alert = function (action) {
 		var self = this;
 
+		// default action is fade
+		action = action || 'fade';
+
+		// current close element
 		this.$close = self.find('.alert__close');
 
+		// click event on close element
 		this.$close.on('click', function () {
+
+			// trigger before close
+			self.trigger('close');
+
+			// current alert element
 			var $alert = $(this).parent('.alert');
 
-			$alert.fadeOut();
+			// animate alert to hide
+			$alert.fadeOut(function () {
+
+				// trigger after close
+				self.trigger('closed');
+
+				// remove alert if requested
+				if (action == 'remove') {
+					$alert.remove();
+				}
+			});
 		});
+
+		return this;
 	};
 })(jQuery);
