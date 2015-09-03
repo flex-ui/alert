da45719257547ac5a2ba60b10e90234b76fdf538
Zura Gabievi <zura.gabievi@singular.ge>
2015-09-03 16:29:59 +0400

    0.1.1

--- alert.js
+++ alert.js
@@ -2,32 +2,32 @@
 	$.fn.alert = function (action) {
 		var self = this;
 
-		// default action is fade
-		action = action || 'fade';
+		// by default dom won't be removed
+		action = action || false;
 
 		// current close element
 		this.$close = self.find('.alert__close');
 
 		// click event on close element
-		this.$close.on('click', function () {
-
-			// trigger before close
-			self.trigger('close');
+		this.$close.on('click', function (e) {
 
 			// current alert element
 			var $alert = $(this).parent('.alert');
+
+			// trigger before close
+			$alert.trigger('close', $alert[0]);
 
 			// animate alert to hide
 			$alert.fadeOut(function () {
 
 				// trigger after close
-				self.trigger('closed');
+				$alert.trigger('closed', $alert[0]);
 
 				// remove alert if requested
-				if (action == 'remove') {
-					$alert.remove();
-				}
+				if (action === true) $alert.remove();
 			});
+
+			e.preventDefault();
 		});
 
 		return this;
